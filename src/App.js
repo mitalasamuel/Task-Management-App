e81048093403eb6273b0bb2ff21d5f816962ae5b
Mitala <mitalatechnologies@gmail.com>
2024-07-18 18:54:49 +0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 // src/App.js
 import React from 'react';
 import AddTask from './components/AddTask';
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="w-full max-w-lg p-6 bg-white rounded-md shadow-md">
+        <img src='/true.webp' alt="Placeholder Image" />
         <h1 className="text-2xl font-bold mb-6 text-center">Task Manager</h1>
         <AddTask />
         <VisibilityFiltersComponent />
