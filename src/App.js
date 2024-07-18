6c42e7f8f0f3b635e47dd403735db81692aa11af
Mitala <mitalatechnologies@gmail.com>
2024-07-18 18:57:04 +0300

    Add

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="w-full max-w-lg p-6 bg-white rounded-md shadow-md">
-        <img src='/true.webp' alt="Placeholder Image" />
         <h1 className="text-2xl font-bold mb-6 text-center">Task Manager</h1>
         <AddTask />
         <VisibilityFiltersComponent />
